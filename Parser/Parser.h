43042e7858b5d9642531e0a2fc8bbf803617189c
elad g <eladgarber@gmail.com>
2020-12-10 16:31:18 +0200

    hard coded configuration

--- Parser/Parser.h
+++ Parser/Parser.h
@@ -2,9 +2,14 @@
 #define PROBLEMSOLVER_PARSER_H
 
 #include <string>
+
 class Parser {
 public:
+    Parser() = default;
+
     virtual std::string parse() = 0;
-    virtual ~Parser()= default;
+
+    virtual ~Parser() = default;
 };
+
 #endif //PROBLEMSOLVER_PARSER_H
--- main.cpp
+++ main.cpp
@@ -3,9 +3,12 @@
 #include "Configuration/Configuration.h"
 
 int main() {
-    vector<pair<string, vector<string>>> solvers = Configuration::readXmlFile("config.xml");
-    vector<string> parser={};
-    Menu *m = new CommandPromptMenu(solvers,parser);
+//    vector<pair<string, vector<string>>> solvers = Configuration::readXmlFile("config.xml");
+    vector<pair<string, vector<string>>> solvers = {pair<string, vector<string>>("MatrixSolver", {"A*"}),
+                                                    pair<string, vector<string>>("MathSolver",
+                                                                                 {"Reverse Polish Interpreter"})};
+    vector<string> parser = {"FileParser", "CommandPromptParser"};
+    Menu *m = new CommandPromptMenu(solvers, parser);
     m->showMenu();
 
     delete m;
